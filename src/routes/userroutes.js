c483aebcfc8bc3201248c10447cc035f7977272e
harishdev07 <hareesh6.2005@gmail.com>
2026-07-25 19:45:52 +0530

    users_routes validatemobile

--- src/routes/userroutes.js
+++ src/routes/userroutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import * as userCtrl from '../controllers/user.js';
 import { verifyToken } from "../middlewares/verify.js";
+import { validateMobile } from "../middlewares/validateNumber.js";
 
 const router = express.Router();
 
 router.get('/user',verifyToken,userCtrl.getUsers);
-router.post('/create', userCtrl.createUsers)
+router.post('/create',validateMobile , userCtrl.createUsers)
 
 export default router;
