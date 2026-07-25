22514fa138cc8326508849cb75a10a19a2e5d477
Spatakal <spatakaltech@gmail.com>
2026-07-25 12:36:21 +0530

    Update server.js remove cron jobs

--- server.js
+++ server.js
@@ -22,44 +22,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to Golden Power Battery API');
 });
 
-// Schedule configuration: 'minute hour day-of-month month day-of-week'
-// 0 9,13,18 * * * means: Minute 0 of Hours 9, 13 (1 PM), and 18 (6 PM) everyday.
-cron.schedule("21 9,13,18,17 * * *", async () => {
-  console.log("Running scheduled service alert check...");
-  await checkAndSendDueAlerts();
-}, {
-  scheduled: true,
-  timezone: "Asia/Kolkata" // Sets execution context to your local time zone
-});
-
-cron.schedule("45 22 * * *", async () => {
-  const { error } = await supabase
-    .from("sessions")
-    .delete()
-    .not("token_hash", "is", null); 
-    // deletes only rows where token_hash is NOT null
-
-  if (error) {
-    console.error("Failed to delete sessions:", error.message);
-  } else {
-    console.log("Sessions with token_hash cleared");
-  }
-});
-
-cron.schedule("19 23 * * *", async () => {
-  const { error } = await supabase
-    .from("service_alert")
-    .update({ status: "due_soon" })
-    .eq("status", "active")
-    .eq("alert_date", new Date(Date.now() + 2 * 24 * 60 * 60 * 1000).toISOString().split("T")[0]);
-
-  if (error) {
-    console.error("Cronjob failed (status):", error.message);
-  } else {
-    console.log("Service alerts updated for upcoming alerts.");
-  }
-});
-
 app.use(errorLogger);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
